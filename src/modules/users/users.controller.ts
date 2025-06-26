94a4709e282167d5010eb3e01caa9ac9e5c1e1f8
Zsebssx <sebastianisle18@gmail.com>
2025-06-26 00:17:07 -0500

    users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -9,37 +9,45 @@ export class UsersController {
 		this.usersService = usersService;
 	}
 
-	create = tryCatch(async (req: Request, res: Response) => {
-		const user = await this.usersService.create(req.body);
-		res.status(201).json(user);
-	});
-
+	
 	findAll = tryCatch(async (req: Request, res: Response) => {
 		const { page, limit } = req.query as { page: string; limit: string };
-		const { items, count, totalPages, hasNextPage } = await this.usersService.findAll({ page, limit });
-		res.status(200).json({ items, count, totalPages, hasNextPage });
+		const { items } = await this.usersService.findAll({ page, limit });
+		res.render("users/list", { items });
 	});
 
-	delete = tryCatch(async (req: Request, res: Response) => {
-		const { id } = req.params;
-		await this.usersService.delete(id);
-		res.status(200).json({ message: "User deleted successfully" });
+	
+	formCreate = tryCatch(async (_req: Request, res: Response) => {
+		res.render("users/create");
+	});
+
+	
+	create = tryCatch(async (req: Request, res: Response) => {
+		await this.usersService.create(req.body);
+		res.redirect("/users");
 	});
 
 	findById = tryCatch(async (req: Request, res: Response) => {
-		const { id } = req.params;
-		const user = await this.usersService.findById(id);
-		res.status(200).json(user);
+		const user = await this.usersService.findById(req.params.id);
+		res.render("users/detail", { user });
 	});
+
+
+	formEdit = tryCatch(async (req: Request, res: Response) => {
+		const user = await this.usersService.findById(req.params.id);
+		res.render("users/edit", { user });
+	});
+
 
 	update = tryCatch(async (req: Request, res: Response) => {
-		const { id } = req.params;
-		const user = await this.usersService.update(id, req.body);
-		res.status(200).json(user);
+		await this.usersService.update(req.params.id, req.body);
+		res.redirect("/users");
+	});
+
+	
+	delete = tryCatch(async (req: Request, res: Response) => {
+		await this.usersService.delete(req.params.id);
+		res.redirect("/users");
 	});
 }
-
-
-
-
 
