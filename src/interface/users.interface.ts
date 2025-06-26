d03a2c1c82fbe4b8817c250785aa323f077850dd
Joseph Infante <josephinfante.y@gmail.com>
2025-06-25 23:43:22 -0500

    adding files

---
+++ src/interface/users.interface.ts
@@ -0,0 +1,13 @@
+export interface User {
+	id: string;
+	first_name: string;
+	last_name: string;
+	email: string;
+	password: string;
+	role_id: string;
+	status: string;
+	created_at: number;
+	updated_at: number | null;
+}
+
+
---
+++ src/modules/users/users.router.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { UsersService } from "./users.service";
+import { UsersController } from "./users.controller";
+
+export class UsersRouter {
+	static get routes(): Router {
+		const router = Router();
+		const service = new UsersService();
+		const controller = new UsersController(service);
+
+		router.post("/", controller.create.bind(controller));
+		router.get("/", controller.findAll.bind(controller));
+		router.delete("/:id", controller.delete.bind(controller));
+		router.get("/:id", controller.findById.bind(controller));
+		router.put("/:id", controller.update.bind(controller));
+
+		return router;
+	}
+}
+
+
+
+
+
